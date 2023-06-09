ba7cfacffd77c691f7b4a4a6123e7ddd2416f809
Thomas <tchoquet@student.42tokyo.jp>
2023-06-09 15:24:27 +0900

    first

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 21:47:36 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/06/02 22:39:23 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/06/09 14:55:11 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 5
 # endif
 
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*get_next_line(int fd);
+size_t	gnl_strlen(const char *s);
+char	*append_buff(char *str, void const *buff, size_t buff_len);
+size_t	first_index(unsigned char value, void *buffer, size_t buffer_len);
+void	memmove_zero(void *dst, void *src, size_t src_len);
 
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_memmove(void *dst, const void *src, size_t len);
+char	*get_next_line(int fd);
 
 #endif
--- main_for_test.c
+++ main_for_test.c
@@ -6,23 +6,26 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 21:47:06 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/06/02 19:51:17 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/06/09 15:23:12 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <libc.h>
 
 int main()
 {
-	int fd = open("test.txt", O_RDONLY);
-
-	char *line;
-
+	char	*line;
+	int		fd;
+	fd = open("test.txt", O_RDONLY);;
 	while (1)
 	{
 		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		printf("len:[%ld]: %s", gnl_strlen(line), line);
+		free(line);
 	}
-	
-
+	close(fd);
 	return 0;
 }
